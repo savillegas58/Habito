f9af3142341b776b59d9e1ec3b7473ce8c265fc8
savillegas58 <savillegas58@gmail.com>
2025-02-06 06:14:24 -0800

    Initial Commit

---
+++ Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by squiggly on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
