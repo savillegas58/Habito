14121c369429291c7c636572bc500cc827386ffb
Joshua Hatch <jshhatch@gmail.com>
2025-02-06 17:05:07 -0500

    added initail creation method and error

--- Habito/Models/DatabaseFoundation.swift
+++ Habito/Models/DatabaseFoundation.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SQLite3
+
+class DatabaseFoundation {
+    static var databaseFoundation = DatabaseFoundation()
+    var db: OpaquePointer?
+    
+    func createDatabase() throws {
+        let dbPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("HabitoDatabase.sqlite").path()
+        
+        if sqlite3_open(dbPath, &db) != SQLITE_OK {
+            throw DatabaseErrors.DatabaseCreationError("Error creating database with path: " + dbPath)
+        }
+    }
+}
