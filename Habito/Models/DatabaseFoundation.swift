b8e29511489189e321b5eacc4061e35d38430fde
Joshua Hatch <jshhatch@gmail.com>
2025-02-07 17:13:23 -0500

    created all remaining table creation methods

--- Habito/Models/DatabaseFoundation.swift
+++ Habito/Models/DatabaseFoundation.swift
@@ -35,7 +35,9 @@ class DatabaseFoundation {
     //TODO: finihs the rest of the table creation methods
     private func createTables() throws {
         try createAccountTable()
-        //createHabitTable()
+        try createHabitTable()
+        try createChallengeTable()
+        try createRecipeTable()
     }
     
     ///createAccountTable()
@@ -45,7 +47,7 @@ class DatabaseFoundation {
     ///Creates a table for storing user accounts with id, username, password, phoneNumber, email, and profilePicutre columns.
     ///id is the primary key and is autoincremented
     private func createAccountTable() throws {
-        let accountTableCreationStatement = "CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, phoneNumber TEXT, email TEXT, profilePictureLink TEXT)"
+        let accountTableCreationStatement = "CREATE TABLE IF NOT EXISTS account(accountID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, phoneNumber TEXT, email TEXT, profilePictureLink TEXT)"
         
         if sqlite3_exec(db, accountTableCreationStatement, nil, nil, nil) != SQLITE_OK {
             let errorMessage = String(cString: sqlite3_errmsg(db)!)
@@ -53,9 +55,49 @@ class DatabaseFoundation {
         }
     }
     
-    //curentProgress and goal will likely be subject to change once we decide what we need for habits
-    /*private func createHabitTable() {
-        let habitTableCreationStatement = "CREATE TABLE IF NOT EXISTS habit(id INTEGER, name TEXT, currentProgress INTEGER, goal INTEGER)"
-        //TODO: foerign key
-    }*/
+    ///createHabitTable()
+    ///No arguments
+    ///no return
+    ///throws: tableCreationError
+    ///Creates a table for storing habits with name, curentProgressm and goal columns and a foreign key referencing the accountID of an account
+    ///columns curentProgress and goal may be subject to change once we knwo what we need for the habits
+    private func createHabitTable() throws {
+        let habitTableCreationStatement = "CREATE TABLE IF NOT EXISTS habit(accountID INTEGER, name TEXT, currentProgress INTEGER, goal INTEGER, FOREIGN KEY(accountID) REFERENCES account(accountID))"
+        
+        if sqlite3_exec(db, habitTableCreationStatement, nil, nil, nil) != SQLITE_OK {
+            let errorMessage = String(cString: sqlite3_errmsg(db)!)
+            throw DatabaseErrors.TableCreationError("Error creating habit table in database: " + errorMessage)
+        }
+    }
+    
+    ///createChallengeTable()
+    ///no arguments
+    ///no return
+    ///throws: table Creation error
+    ///Creates a table for storing challenges with name, curentProgress, goal, and date columns and a foreign key referencing the accountID of an account
+    ///Date is stores as Text becaus SQLite doesnt not have a dedicate date type
+    private func createChallengeTable() throws {
+        let challengeTableCreationStatement = "CREATE TABLE IF NOT EXISTS challenge(accountID INTEGER, name TEXT, currentProgress INTEGER, goal INTEGER, date TEXT, FOREIGN KEY(accountID) REFERENCES account(accountID))"
+        
+        if sqlite3_exec(db, challengeTableCreationStatement, nil, nil, nil) != SQLITE_OK {
+            let errorMessage = String(cString: sqlite3_errmsg(db)!)
+            throw DatabaseErrors.TableCreationError("Error creating challenge habit table in database: " + errorMessage)
+        }
+    }
+    
+    ///createRecipeTable()
+    ///no argument
+    ///no return
+    ///throws: tabelCreationError
+    ///creates a table for stroing recipes with the columns anme, ingridients, suggestions, timeInMinutes, calories, and rating
+    ///recipes are not assocaited with accounts
+    private func createRecipeTable() throws {
+        let recipeTableCreationStatment = "CREATE TABLE IF NOT EXISTS recipe(name TEXT, ingredients TEXT, suggestions TEXT, timeInMinutes INTEGER, calories INTEGER, rating INTEGER)"
+        
+        if sqlite3_exec(db, recipeTableCreationStatment, nil, nil, nil) != SQLITE_OK {
+            let errorMessage = String(cString: sqlite3_errmsg(db)!)
+            throw DatabaseErrors.TableCreationError("Error creating recipe table in database: " + errorMessage)
+        }
+    }
+    
 }
