1359bf77395368ed34d787fc83a436d8b4cdbd37
Joshua Hatch <jshhatch@gmail.com>
2025-02-10 11:56:19 -0500

    added tests for data fetcher. Also renamed a atest in DatabaseinserterTest

--- HabitoTests/DatabseAccountFetcherTest.swift
+++ HabitoTests/DatabseAccountFetcherTest.swift
@@ -12,13 +12,32 @@ final class DatabseAccountFetcherTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try DatabaseFoundation.databaseFoundation.createDatabase()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try DatabaseAccountDeleter.accountDeleter.deleteAllAccounts()
+    }
+    
+    func populateDataBaseWithAccounts(numberOfAccounts: Int) throws {
+        for i in 0..<numberOfAccounts {
+            let accountSpecificData : NSString = ("test: " + String(i)) as NSString
+            try DatabaseInserter.databaseInserter.insertAccount(username: accountSpecificData, password: accountSpecificData, phoneNumber: accountSpecificData, email: accountSpecificData)
+        }
     }
 
     func testFetchAllAccounts_noThrows_noAccountsInDatabase() throws {
-        
+        XCTAssertNoThrow(try DatabaseAccountFetcher.accountFetcher.fetchAllAccounts())
+    }
+    
+    func testFetchAllAccounts_returns_empty_array_noAccountsInDataBase() throws {
+        XCTAssertEqual(try DatabaseAccountFetcher.accountFetcher.fetchAllAccounts().count, 0)
+    }
+    
+    func testFetchAllAccounts_returnsArrayWith2Accountsa_2AccountsInDatabase() throws {
+        try populateDataBaseWithAccounts(numberOfAccounts: 2)
+        let fetchResult = try DatabaseAccountFetcher.accountFetcher.fetchAllAccounts()
+        XCTAssertEqual(fetchResult.count, 2)
     }
 }
