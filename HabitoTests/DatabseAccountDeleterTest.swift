827aaaac7ee38ee3b91c45fce5df664b7de6705e
Joshua Hatch <jshhatch@gmail.com>
2025-02-10 14:20:16 -0500

    can now delete specified accounts

--- HabitoTests/DatabseAccountDeleterTest.swift
+++ HabitoTests/DatabseAccountDeleterTest.swift
@@ -17,10 +17,28 @@ final class DatabseAccountDeleterTest: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try DatabaseAccountDeleter.accountDeleter.deleteAllAccounts()
     }
-
+    
+    func populateDataBaseWithAccounts(numberOfAccounts: Int) throws {
+        for i in 0..<numberOfAccounts {
+            let accountSpecificData : NSString = ("test: " + String(i)) as NSString
+            try DatabaseInserter.databaseInserter.insertAccount(username: accountSpecificData, password: accountSpecificData, phoneNumber: accountSpecificData, email: accountSpecificData)
+        }
+    }
+    
     func testDeleteAllAccounts_noThrows_NoAccountsInDatabase() throws {
         XCTAssertNoThrow(try DatabaseAccountDeleter.accountDeleter.deleteAllAccounts())
+    }
+    
+    func testDeleteByUsername_removesSpecifiedAccount_accountToDeleteExistsInDatabase() throws {
+        try populateDataBaseWithAccounts(numberOfAccounts: 3)
+        
+        let accountUsernameToDelete = "test: 2"
+        DatabaseAccountDeleter.accountDeleter.deleteAccountByName(username: accountUsernameToDelete)
+        
+        XCTAssertNil(DatabaseAccountFetcher.accountFetcher.fetchAccountByUsername(username: accountUsernameToDelete))
+        
     }
 
 }
