55656cca645c6c37b00caf4faf0d9ef664bad234
Joshua Hatch <jshhatch@gmail.com>
2025-02-11 16:38:52 -0500

    habits can noew have their progress updated

---
+++ HabitoTests/HabitTests/DatabasseHabitUpdateTest.swift
@@ -0,0 +1,38 @@
+//
+//  DatabasseHabitUpdateTest.swift
+//  HabitoTests
+//
+//  Created by admin on 2/11/25.
+//
+
+import XCTest
+@testable import Habito
+
+final class DatabasseHabitUpdateTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try DatabaseFoundation.databaseFoundation.createDatabase()
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try DatabaseAccountDeleter.accountDeleter.deleteAllAccounts()
+    }
+
+    func testUpdateAccountHabitProgeress_habitIsUpdated_accountAndHAbitExist() throws {
+        let testString : NSString = "test"
+        try DatabaseAccountInserter.accountInserter.insertAccount(username: testString, password: testString, phoneNumber: testString, email: testString)
+        
+        let fetchedAccount = DatabaseAccountFetcher.accountFetcher.fetchAccountByUsername(username: testString as String)
+        
+        try DatabaseHabitInserter.habitInserter.insertHabit(accountID: (fetchedAccount?.ID)!, name: "testHabit", currentProgress: 0, goal: 10)
+        
+        try DatabaseHabitUpdater.habitUpdater.updateAccountHabitProgress(accountID: (fetchedAccount?.ID)!, habitName: "testHabit", newProgress: 5)
+        
+        let fetchedHabits = try DatabaseHabitFetcher.habitFetcher.fetchAccountHabits(accountID: (fetchedAccount?.ID)!)
+        
+        
+        XCTAssertEqual(fetchedHabits[0].currentProgress, 5)
+    }
+}
