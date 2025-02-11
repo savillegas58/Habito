04fa9899a7880ceab9f9b625a5aa0ab35c47bf45
Joshua Hatch <jshhatch@gmail.com>
2025-02-11 15:58:33 -0500

    added habit deletion. God I just want this database stuff to be done already

---
+++ HabitoTests/HabitTests/DatabaseHabitDeleterTest.swift
@@ -0,0 +1,45 @@
+//
+//  DatabaseHabitDeleterTest.swift
+//  HabitoTests
+//
+//  Created by admin on 2/11/25.
+//
+
+import Foundation
+import XCTest
+@testable import Habito
+
+final class DatabaseHabitDeleterTest: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try DatabaseFoundation.databaseFoundation.createDatabase()
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try DatabaseAccountDeleter.accountDeleter.deleteAllAccounts()
+    }
+    
+    func populateDataBaseWithAccounts(numberOfAccounts: Int) throws {
+        for i in 0..<numberOfAccounts {
+            let accountSpecificData : NSString = ("test: " + String(i)) as NSString
+            try DatabaseAccountInserter.accountInserter.insertAccount(username: accountSpecificData, password: accountSpecificData, phoneNumber: accountSpecificData, email: accountSpecificData)
+        }
+    }
+    
+    func testDeleteAllHabitsFromAccount_noThrow_accountExists() throws {
+        try populateDataBaseWithAccounts(numberOfAccounts: 1)
+        let fetchedAccount = DatabaseAccountFetcher.accountFetcher.fetchAccountByUsername(username: "test: 0")
+        let accountID = fetchedAccount?.ID
+        
+        try DatabaseHabitInserter.habitInserter.insertHabit(accountID: accountID!, name: "habit" as NSString, currentProgress: 1, goal: 10)
+        try DatabaseHabitInserter.habitInserter.insertHabit(accountID: accountID!, name: "habit1" as NSString, currentProgress: 3, goal: 10)
+        
+        try DatabaseHabitDeleter.habitDeleter.deleteAllHabitsFromAccount(accountID: accountID!)
+        
+        let fetchedHabits = try DatabaseHabitFetcher.habitFetcher.fetchAccountHabits(accountID: accountID!)
+        
+        XCTAssertEqual(fetchedHabits.count, 0)
+    }
+}
