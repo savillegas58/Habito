d176395c2ea9ab29761c7d2d910f94c0bc2d2ab5
Joshua Hatch <jshhatch@gmail.com>
2025-02-13 14:14:49 -0500

    added navigation between the  between onboarding

--- Habito/Views/Onboarding/Onboarding2.swift
+++ Habito/Views/Onboarding/Onboarding2.swift
@@ -8,30 +8,62 @@
 import SwiftUI
 
 struct Onboarding2: View {
+    @State var showViewThree = false
     var body: some View {
-        VStack{
+        if showViewThree {
+            //this flase might need to chagne?
+            Onboarding3(showSignIn: false)
+        } else {
+            VStack{
+                HStack{
+                    Text("Achieve")
+                        .bold()
+                    Text("your goals")
+                        .bold()
+                        .foregroundStyle(.darkGreen)
+                    Text("easily!")
+                        .bold()
+                }.padding()
+                Text("\"We give you simple tools to track your")
+                    .bold()
+                    .foregroundStyle(.gray)
+                Text("habits and analyze you health")
+                    .bold()
+                    .foregroundStyle(.gray)
+                Text("progress\"")
+                    .foregroundStyle(.gray)
+                    .bold()
+            }
+            Spacer()
             HStack{
-                Text("Achieve")
-                    .bold()
-                Text("your goals")
-                    .bold()
-                    .foregroundStyle(.darkGreen)
-                Text("easily!")
-                    .bold()
-            }.padding()
-            Text("\"We give you simple tools to track your")
-                .bold()
-                .foregroundStyle(.gray)
-            Text("habits and analyze you health")
-                .bold()
-                .foregroundStyle(.gray)
-            Text("progress\"")
-                .foregroundStyle(.gray)
-                .bold()
+                Spacer()
+                ArrowButton2(showViewThree: $showViewThree)
+            }
         }
     }
 }
 
+//this redclartion iss pretty lazy of me but if i have time I'll refactor
+struct ArrowButton2: View {
+    @Binding var showViewThree : Bool
+    var body : some View {
+        Button(action: {
+            print("Arrow button pushed")
+            showViewThree = true
+        }) {
+            Image(systemName: "arrow.right")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.grayishWhite)
+                .frame(width: 30, height: 70)
+                .padding()
+        }.background(
+            Circle()
+                .fill(.customDarkGray)
+                .frame(width: 70, height: 70)
+        ).padding()
+    }
+}
 #Preview {
     Onboarding2()
 }
