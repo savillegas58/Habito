42d55d5b9b4bdc152b70fad45e370bef7079f029
Joshua Hatch <jshhatch@gmail.com>
2025-02-16 14:27:50 -0500

    added method to view model

--- Habito/ViewModels/DataAnalyzer.swift
+++ Habito/ViewModels/DataAnalyzer.swift
@@ -11,16 +11,20 @@ class DataAnalyzer {
     //will this update?
     let currentAccountID = UserDefaults.standard.integer(forKey: "currentAccountID")
     let walkingHabitList : [Habit]
+    let sleepingHabitList : [Habit]
     
     init() {
         var fetchedWalkingHabits = [Habit]()
+        var fetchedSleepingHabits = [Habit]()
         
         do {
             fetchedWalkingHabits = try DatabaseHabitFetcher.habitFetcher.fetchWalkingHabits(accountID: currentAccountID)
+            fetchedSleepingHabits = try DatabaseHabitFetcher.habitFetcher.fetchSleepingHabits(accountID: currentAccountID)
         } catch {
             print("Error fetching habits for Data Analyzer: \(error)")
         }
         self.walkingHabitList = fetchedWalkingHabits
+        self.sleepingHabitList = fetchedSleepingHabits
     }
     
     //this currently wokrs under the assumption that 1 'point' of progress is 1000 steps
@@ -35,6 +39,10 @@ class DataAnalyzer {
     }
     
     func calcualteSleep() -> Int{
+        var totalSleepHours = 0
+        
+        
+        
         return 0
     }
 }
