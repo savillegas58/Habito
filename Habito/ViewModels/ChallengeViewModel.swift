89aea599e12e9af3b765c0a0285a93232ada9412
Joshua Hatch <jshhatch@gmail.com>
2025-02-15 20:03:43 -0500

    added challenge fetching

--- Habito/ViewModels/ChallengeViewModel.swift
+++ Habito/ViewModels/ChallengeViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class ChallengeViewModel {
+    func getAccountChalenges() -> [Challenge] {
+        let currentAccountID = UserDefaults.standard.integer(forKey: "currentAccountID");
+        var challegneList = [Challenge]()
+        
+        do {
+            challegneList = try DatabaseChallengeFetcher.challengeFetcher.fetchAccountsChallenges(accountID: currentAccountID)
+        } catch {
+            print("Error fetching challenges in ChallengeViewModel")
+        }
+        
+        return challegneList
+    }
+}
